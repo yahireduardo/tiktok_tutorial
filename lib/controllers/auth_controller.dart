6a1675215922fbf28f009e8cf89dd160e5bc2614
yahireduardo <Yahireduardo@hotmail.com>
2025-12-02 07:41:40 -0600

    initial commit

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_tutorial/constants.dart';
+import 'package:tiktok_tutorial/models/user.dart' as model;
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+
+  late Rx<File?> _pickedImage;
+
+  File? get profilePhoto => _pickedImage.value;
+
+  void pickedImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedImage != null) {
+      Get.snackbar(
+        "Profile Picture",
+        "You have successfully selected your profile picture!",
+      );
+    }
+    _pickedImage = Rx<File?>(File(pickedImage!.path));
+  }
+
+
+
+  //upload to firebase storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+      .ref()
+      .child('profilePics')
+      .child(firebaseAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // registering the user
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try{
+      if(username.isNotEmpty &&
+      email.isNotEmpty &&
+      password.isNotEmpty &&
+      image != null) {
+      // save out user to our auth and firebase firestore
+      UserCredential cred = await firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+        model.User user = model.User(
+          name: username,
+          profilePhoto: downloadUrl,
+          email: email,
+          uid: cred.user!.uid,
+        );
+        await firestore.collection('users').doc(cred.user!.uid).set(user.toJson());
+      } else {
+        Get.snackbar(
+          "Error Creating Account",
+          "Please enter all the fields",
+          );
+      }
+    } catch (e) {
+    Get.snackbar(
+      "Error Creating Account",
+      e.toString(),
+      );
+    }
+  void loginUser(String email, String password) async {
+    try {email.isNotEmpty &&
+      password.isNotEmpty)
+    } catch (e) {
+      Get.snackbar(
+        "Error Logging In",
+        e.toString(),
+      );
+  }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_tutorial/constants.dart';
+import 'package:tiktok_tutorial/controllers/auth_controller.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tiktok Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: SingupScreen(),
+    );
+  }
+}
+
+class SingupScreen extends StatelessWidget {
+  const SingupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signup'),
+      ),
+      body: const Center(
+        child: Text('Signup Screen'),
+      ),
+    );
+  }
+}
